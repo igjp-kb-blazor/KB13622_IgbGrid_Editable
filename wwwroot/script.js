ff070a70cdccb3e958574a171eea9f687db1f8a8
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-05-24 10:12:22 +0900

    初回コミット

---
+++ wwwroot/script.js
@@ -0,0 +1,15 @@
+﻿//wwwroot/script.jsをインポートします。
+// セルが編集モードに入るときに発生します
+igRegisterScript("WebGridCellEditEnter", (event) => {
+    event.detail.cancel = !event.detail.rowData.Editabile;
+}, false);
+
+// 特定の列自体の編集を制限すると下記の様になります
+//igRegisterScript("WebGridCellEditEnter", (event) => {
+//    // 編集しようとしている列のフィールド名を取得
+//    const editingField = event.detail.column.field;
+//    // 特定の列（ここでは 'JobTitle'列）の編集を制限
+//    if (editingField === 'JobTitle') {
+//        event.detail.cancel = !event.detail.rowData.Editabile;
+//    }
+//}, false);
